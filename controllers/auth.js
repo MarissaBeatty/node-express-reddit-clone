e0bd531b47d4d2fd1f4478d9154ef28f2c3d9364
Marissa Beatty <beattymarissa@gmail.com>
2017-07-20 01:55:45 +0000

    created signup and login forms with pug. Signup works and sends data to database. Login form almost works but need to implement cookie sessions

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,20 +4,33 @@ module.exports = function(myReddit) {
     var authController = express.Router();
     
     authController.get('/login', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
+        response.render('login-form');
     });
     
     authController.post('/login', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
-    });
+        myReddit.checkUserLogin(request.body)
+       if(Error.code === '401') {
+           throw new Error('401 Unauthorized');
+       } else {
+           var userId = myReddit.checkUserLogin.userId
+        //   res.send(myReddit.checkUserLogin.username);
+            myReddit.createUserSession(userId)
+            .then(response.cookie('SESSION', response.createUserSession))
+            .then(response.redirect('homepage'))
+        }
+    })
     
     authController.get('/signup', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
+        response.render('signup-form');
     });
     
     authController.post('/signup', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
+        myReddit.createUser(request.body) 
+        .then (myReddit.createUser.response) 
+        .then(response.redirect('/auth/login'));
     });
-    
+   
     return authController;
-}
+};
+
+
